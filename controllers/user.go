1ba9b98a44399998861f4b53872a35d3aae77a33
Gaurav Tak <gauravtak996@gmail.com>
2025-01-09 00:53:47 +0530

    feat: func GetUserById added

--- controllers/user.go
+++ controllers/user.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 	"go-sql/db"
+	"log"
+	"strings"
 	"fmt"
 )
 
@@ -55,4 +57,25 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Println("User created")
+}
+
+func GetUserById(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.Path, "/")
+	if len(parts) < 3 {
+		http.Error(w, "Invalid URL format", http.StatusBadRequest)
+		return
+	}
+	id := parts[2]
+	query := "SELECT id,name,email FROM users WHERE id=?;"
+	var u User
+	err := db.DB.QueryRow(query, id).Scan(&u.Id, &u.Name, &u.Email)
+	if err != nil {
+		log.Println("Error fetching user:", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(u)
+	if err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+	}
 }
