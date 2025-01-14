daa755d5928bc56e96d96d1bf3b664933ede1669
Gaurav Tak <gauravtak996@gmail.com>
2025-01-14 17:34:46 +0530

    update: styles for CreateUserForm and add to main layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,23 +3,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios'
+import CreateUserForm from "./Forms/CreateUserForm"
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [user, setUser] = useState(null)
+  const [showCreateUserForm, setShowCreateUserForm] = useState(false)
   const fetchAll = async ()=>{
     try {
       const res = await axios.get("http://localhost:4000/users") 
-      console.log(res.data)
+      setUsers(res.data)
     } catch(err) {
       console.error("Unable to fetch all users", err)
     }
   }
 
-  const fetchById = ()=>{1
-    console.log("Clicked 2")
+  const fetchById = async ()=>{
+    try {
+      console.log("Fetching user with user id 1")
+      const res = await axios.get("http://localhost:4000/user/1") 
+      setUser(res.data)
+    } catch(err) {
+      console.error("Unable to fetch all users", err)
+    }
   }
 
-  const handleClick3 = ()=>{1
-    console.log("Clicked 3")
+  const makeFormAppear = ()=>{
+    setShowCreateUserForm(true)
   }
 
   return (
@@ -28,7 +38,30 @@ function App() {
       <div className="button-container">
         <button onClick={fetchAll}>fetch all users</button> 
         <button onClick={fetchById}>fetch user by id</button> 
-        <button onClick={handleClick3}>create user</button> 
+        <button onClick={makeFormAppear}>create user</button> 
+      </div> 
+      {/*fetching all users*/}
+      <div>
+        {users && users.map(user=>{
+          return(
+            <div key={user.id}>
+              <p>{user.name}</p> 
+              <p>{user.email}</p> 
+            </div> 
+          )
+        })}
+      </div> 
+      {/*fetching user by id*/}
+      <div>
+        {user && (
+          <div>
+            <p>{user.name}</p> 
+            <p>{user.email}</p> 
+          </div> 
+        )} 
+      </div> 
+      <div>
+        {showCreateUserForm && <CreateUserForm/>}
       </div> 
     </div> 
   )
